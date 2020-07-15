4f0f6e0b229596b55ad53d890e19fc7d0c8ed533
Joseph Johnson <jwjbadger@gmail.com>
2020-07-15 05:01:17 -0500

    Add Header

--- cookbook-react/src/App.jsx
+++ cookbook-react/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Cookbook from './Components/Cookbook/Cookbook';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Cookbook />
     </div>
   );
---
+++ cookbook-react/src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='Head'>
+        <h2>The Cookbook</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
