23f8366ccfb2668d65444c9b4eb23228b8ac0eb6
Joseph Johnson <jwjbadger@gmail.com>
2020-07-16 05:49:11 -0500

    Add basic list view for recipes

--- cookbook_flutter/lib/main.dart
+++ cookbook_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'Recipe.dart';
 import 'RecipeService.dart';
 
 void main() {
@@ -33,7 +34,89 @@ class _RecipesPageState extends State<RecipesPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Recipes')),
+      appBar: AppBar(
+          title: Text('Recipes',
+              style: TextStyle(color: Theme.of(context).accentColor))),
+      body: FutureBuilder(
+          future: _recipeService.getRecipes(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Recipe>> snapshot) {
+            if (snapshot.hasData) {
+              List<Recipe> recipes = snapshot.data;
+
+              return ListView.builder(
+                itemCount: recipes.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Recipe recipe = recipes[index];
+
+                  return Padding(
+                    padding: EdgeInsets.only(top: 3, left: 5, right: 5),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        color: Theme.of(context).accentColor,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 4, top: 2),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  child: Text(
+                                    recipe.title,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  child: Text(
+                                    'Serves ${recipe.servings},',
+                                    style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 10,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  child: Text(
+                                    'By ${recipe.author},',
+                                    style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 10,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              recipe.description,
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                fontSize: 10,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            return Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
